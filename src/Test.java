8059a152cc0e05f665efb4f3894d139ddd2a8d5a
Tean Wong <wangtyang@gmail.com>
2016-09-11 13:10:42 -0400

    Test version 1.0

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,41 @@
+import edu.mcw.rgd.common.utils.FileEntry;
+import edu.mcw.rgd.common.utils.FileList;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 /**
  * Created by wangt on 8/25/2016.
  */
 public class Test {
+    private String filePath = "";
+    public ArrayList<FileEntry> fileList = new ArrayList();
 
+    public int save() throws Exception {
+        try {
+            BufferedWriter e = new BufferedWriter(new FileWriter(this.filePath));
+
+            for(int i = 0; i < this.fileList.size(); ++i) {
+                FileEntry fe = this.fileList.get(i);
+                e.write(fe.toString());
+                e.newLine();
+            }
+
+            e.close();
+            return this.fileList.size();
+        } catch (Exception var4) {
+            throw var4;
+        }
+    }
+
+    public static void main(String[] args){
+
+
+
+
+    }
 
 
 
